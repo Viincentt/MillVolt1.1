07e85330df907a99481cf3f9dcafd4947d49bf1d
fourbeplayer <tristan.martin@epita.fr>
2017-02-16 17:59:10 +0100

    Millvolt V1.1.1

---
+++ MillVolt/Assets/Scripts/CarMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.UpArrow)) { transform.Translate(1, 0, 0); }
+        if (Input.GetKey(KeyCode.DownArrow)) { transform.Translate(-1, 0, 0); }
+        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Rotate(0, -5, 0); }
+        if (Input.GetKey(KeyCode.RightArrow)) { transform.Rotate(0, 5, 0); }
+    }
+}
